cdc3f9b8339d2b7fbfdc4637b97aca40ea0f9d71
Redstone Chicken <94724551+RedstoneChicken@users.noreply.github.com>
2025-06-24 10:15:45 +0800

    vite and node js

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/redstonechicken.github.io/' // Replace with your GitHub repo name
 }));
